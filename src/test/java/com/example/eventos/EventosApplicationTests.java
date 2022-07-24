3046c06e7ad01d23f30682932b96d828e39a4f62
Joan <joanjube95@gmail.com>
2022-07-24 18:34:57 +0200

    EventosApplication tests

--- src/test/java/com/example/eventos/EventosApplicationTests.java
+++ src/test/java/com/example/eventos/EventosApplicationTests.java
@@ -1,9 +1,17 @@
 package com.example.eventos;
 
 import com.example.eventos.calendario.CalendarioController;
+import com.example.eventos.evento.EventoController;
+import com.example.eventos.invitado.InvitadoController;
+import com.example.eventos.mesa.MesaController;
+import com.example.eventos.protagonista.ProtagonistaController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.TimeZone;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -12,9 +20,47 @@ class EventosApplicationTests {
 	@Autowired
 	private CalendarioController calendarioController;
 
+	@Autowired
+	private EventoController eventoController;
+
+	@Autowired
+	private InvitadoController invitadoController;
+
+	@Autowired
+	private MesaController mesaController;
+
+	@Autowired
+	private ProtagonistaController protagonistaController;
+
 	@Test
-	void contextLoads() {
+	void contextLoadsCalendarioController() {
 		assertNotNull(calendarioController);
+	}
+
+	@Test
+	void contextLoadsEventoController() {
+		assertNotNull(eventoController);
+	}
+
+	@Test
+	void contextLoadsInvitadoController() {
+		assertNotNull(invitadoController);
+	}
+
+	@Test
+	void contextLoadsMesaController() {
+		assertNotNull(mesaController);
+	}
+
+	@Test
+	void contextLoadsProtagonistaController() {
+		assertNotNull(protagonistaController);
+	}
+
+	@Test
+	void postConstructSetTimezoneToUTC() {
+		TimeZone defaultTimeZone = TimeZone.getDefault();
+		assertEquals(defaultTimeZone.getID(), "UTC");
 	}
 
 }
