a54533682eadb9859fe843d0aeeb92269cf5b33b
Joan <joanjube95@gmail.com>
2022-08-21 14:02:10 +0200

    Security system

--- src/main/java/com/example/eventos/security/WebSecurity.java
+++ src/main/java/com/example/eventos/security/WebSecurity.java
@@ -1,2 +1,37 @@
-package com.example.eventos.security;public class WebSecurity {
+package com.example.eventos.security;
+
+import com.example.eventos.usuario.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usuarioService);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/anyadirEvento").hasRole("ADMIN")
+                .antMatchers("/verEventos").hasRole("USUARIO")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
