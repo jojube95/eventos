75f7561e7f062efe1263cd5036a261aac9172880
Joan <joanjube95@gmail.com>
2022-08-31 13:45:01 +0200

    Add modal confirm delete Empleado

---
+++ src/main/java/com/example/eventos/empleado/EmpleadoRestController.java
@@ -0,0 +1,23 @@
+package com.example.eventos.empleado;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+
+@RestController
+public class EmpleadoRestController {
+
+    private final EmpleadoService empleadoService;
+
+    public EmpleadoRestController(EmpleadoService empleadoService) {
+        this.empleadoService = empleadoService;
+    }
+
+    @PostMapping("/eliminarEmpleado")
+    public Empleado delete(@RequestParam("empleadoId") String empleadoId) throws IOException {
+        Empleado empleado = empleadoService.getById(empleadoId);
+        empleadoService.delete(empleado);
+        return empleado;
+    }
+}
