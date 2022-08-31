0936888bd5f1009823d27e5ef89d73878c4f675b
Joan <joanjube95@gmail.com>
2022-08-31 14:02:55 +0200

    Add permision

--- src/main/java/com/example/eventos/eventoEmpleado/EventoEmpleadoController.java
+++ src/main/java/com/example/eventos/eventoEmpleado/EventoEmpleadoController.java
@@ -7,9 +7,7 @@ import com.example.eventos.evento.EventoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.List;
 
 @Controller
