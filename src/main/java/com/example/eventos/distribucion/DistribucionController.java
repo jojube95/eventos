8fe0a1b14445ee4412fade070c714f6af7ac128b
Joan <joanjube95@gmail.com>
2022-09-04 19:50:17 +0200

    Distribution drawer

---
+++ src/main/java/com/example/eventos/distribucion/DistribucionController.java
@@ -0,0 +1,39 @@
+package com.example.eventos.distribucion;
+
+import com.example.eventos.evento.EventoService;
+import com.example.eventos.invitado.Invitado;
+import com.example.eventos.mesa.Mesa;
+import com.example.eventos.mesa.MesaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+
+@Controller
+public class DistribucionController {
+    private final MesaService mesaService;
+
+    private final EventoService eventoService;
+
+    public DistribucionController(MesaService mesaService, EventoService eventoService) {
+        this.mesaService = mesaService;
+        this.eventoService = eventoService;
+    }
+
+    @GetMapping("/evento/distribucion")
+    public String distribucion(@RequestParam("eventoId") String eventoId, Model model) {
+        List<Mesa> mesas = mesaService.findByEvento(eventoId);
+        model.addAttribute("idEvento", eventoId);
+        model.addAttribute("mesas", mesas);
+        return "distribucion";
+    }
+
+    @GetMapping("/evento/distribucion/tipoMesaModal")
+    public String modalTipoMesa(@RequestParam("mesaId") String mesaId, @RequestParam("numero") String numero, @RequestParam("personas") String personas, Model model){
+        model.addAttribute("mesaId", mesaId);
+        model.addAttribute("numero", numero);
+        model.addAttribute("personas", personas);
+        return "fragments/distribucionTipoMesaModal :: modalContents";
+    }
+}
