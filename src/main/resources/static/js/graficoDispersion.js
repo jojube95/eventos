25021e4b7170d86dd884fa16ad30451359fd2c90
jojube95 <joanjube95@gmail.com>
2023-03-03 09:02:34 +0100

    Finish DispersionChart

--- src/main/resources/static/js/graficoDispersion.js
+++ src/main/resources/static/js/graficoDispersion.js
@@ -7,84 +7,45 @@ document.addEventListener('DOMContentLoaded', function() {
         orientation: "bottom auto",
         autoclose: true
     }).change(function () {
-        drawChart($(this).val(), $("#dato").val());
+        drawChart($(this).val());
     });
 
     $("#datepicker").val(new Date().getFullYear());
 
-    $("#dato").change(function() {
-        drawChart($("#datepicker").val(), $(this).val());
-    });
-
     google.charts.load('current', {'packages':['corechart']});
 
     google.charts.setOnLoadCallback(function () {
-        drawChart(2023, 'comensales')
+        drawChart(2023)
     });
 });
 
-function drawChart(year, dato) {
-    //let data = prepareData(year, dato);
-
-    let data = google.visualization.arrayToDataTable([
-        ['ID', 'Life Expectancy', 'Fertility Rate', 'Region', 'Size'],
-        ['',    150,              40,      'North America', 5],
-        ['',    125,              42,      'Europe', 5],
-        ['',    50,               40,      'Europe', 5],
-        ['',    60,              45,      'Middle East', 5],
-        ['',    80.05,              47,         'Europe', 5],
-        ['',    150,              80,       'Middle East', 5],
-        ['',    56,              35,      'Middle East', 5],
-        ['',    126,              45,      'Middle East', 5],
-        ['',    67,               43,      'Europe', 5],
-        ['',    69,              45,      'North America', 5]
-    ]);
+function drawChart(year) {
+    let data = prepareData(year);
 
     let options = {
         width: 800,
         height: 600,
-        title: 'Fertility rate vs life expectancy in selected countries (2010).' +
-            ' X=Life Expectancy, Y=Fertility, Bubble size=Population, Bubble color=Region',
-        hAxis: {title: 'Life Expectancy'},
-        vAxis: {title: 'Fertility Rate'}
+        title: 'Comenslaes vs precio menú.',
+        hAxis: {title: 'Precio Menú'},
+        vAxis: {title: 'Comensales'}
     };
 
     let chart = new google.visualization.BubbleChart(document.getElementById('chart_div'));
     chart.draw(data, options);
 }
 
-function prepareData(year, dato) {
-    let groups = eventos.reduce(function (res, act) {
-        if(new Date(act.fecha).getFullYear() === Number(year)) {
-            let tipo = act.tipo.value;
-            let valor;
-            if(dato === 'comensales'){
-                valor = act.personas.mayores;
-            }
-            else{
-                valor = Number(act.personas.mayores) * Number(act.precioMenu) * 0.325;
-            }
-
-            if(res[tipo]) {
-                res[tipo] += valor;
-            }
-            else{
-                res[tipo] = valor;
-            }
-        }
-        return res;
-    }, {});
-
-
-
-    let headerRow = ["Tipo", "Valor"];
+function prepareData(year) {
+    let headerRow = ['ID', 'Comensales', 'Precio Menú', 'Tipo', 'Beneficio'];
 
     let filas = [];
-    Object.keys(groups).forEach(key => {
-        filas.push([key].concat(groups[key]));
-    });
 
-    filas.unshift(headerRow);
+    filas.push(headerRow);
+
+    eventos.forEach(evento => {
+        if(new Date(evento.fecha).getFullYear() === Number(year)) {
+            filas.push(['', evento.personas.mayores, evento.precioMenu, evento.tipo.value, (evento.personas.mayores * evento.precioMenu) * 0.325]);
+        }
+    });
 
     return google.visualization.arrayToDataTable(filas);
 }
