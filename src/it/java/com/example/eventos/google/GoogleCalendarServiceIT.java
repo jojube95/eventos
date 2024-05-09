cb0ebc436a5b092544b70f476d1f01bdac01a72f
jojube95 <joanjube95@gmail.com>
2024-05-09 19:23:07 +0200

    Repair google calendar it test

--- src/it/java/com/example/eventos/google/GoogleCalendarServiceIT.java
+++ src/it/java/com/example/eventos/google/GoogleCalendarServiceIT.java
@@ -23,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { GoogleCalendarService.class })
-@EnableConfigurationProperties
 class GoogleCalendarServiceIT {
 
     @Autowired
