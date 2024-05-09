026d1921eeb8ce1f49099d680dd5b1acd685e904
jojube95 <joanjube95@gmail.com>
2024-05-09 18:17:31 +0200

    Add test property source

--- src/it/java/com/example/eventos/horarioEvento/HorarioEventoRepositoryIT.java
+++ src/it/java/com/example/eventos/horarioEvento/HorarioEventoRepositoryIT.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.TestPropertySource;
+
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataMongoTest
+@TestPropertySource(locations = "classpath:application-integration.properties")
 class HorarioEventoRepositoryIT {
 
     @Autowired
--- src/it/java/com/example/eventos/parametros/ParametrosRepositoryIT.java
+++ src/it/java/com/example/eventos/parametros/ParametrosRepositoryIT.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.TestPropertySource;
+
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataMongoTest
+@TestPropertySource(locations = "classpath:application-integration.properties")
 class ParametrosRepositoryIT {
 
     @Autowired
